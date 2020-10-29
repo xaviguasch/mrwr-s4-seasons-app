621a3997bbd13b0adb3adc2404dfcca4dcfe4747
xaviguasch <xaviguasch@gmail.com>
2020-10-29 15:46:56 +0100

    refactored App component to a class based one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  )
+import React from 'react'
 
-  return <div>Hi There</div>
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    )
+
+    return <div>Hi There</div>
+  }
 }
 
 export default App
