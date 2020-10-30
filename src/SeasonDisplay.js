1ccb2962ca319e5e97d5c50ed93ba379b097f040
xaviguasch <xaviguasch@gmail.com>
2020-10-30 20:48:15 +0100

    added logic inside SeasonDisplay to determine the season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
+const getSeason = (lat, month) => {
+  if (month >= 3 && month <= 8) {
+    return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat > 0 ? 'winter' : 'summer'
+  }
+}
+
 const SeasonDisplay = (props) => {
-  return <div>Latitude: {props.lat}</div>
+  const season = getSeason(props.lat, new Date().getMonth())
+
+  return <div>{season}</div>
 }
 
 export default SeasonDisplay
