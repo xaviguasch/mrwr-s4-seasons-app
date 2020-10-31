a9473fcf050a2e6a29ce1dad7b4a2786f028e58a
xaviguasch <xaviguasch@gmail.com>
2020-10-31 13:44:01 +0100

    added a Spinner component while the app waits on loading the geolocation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' }
@@ -18,7 +19,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <div>loading!</div>
+    return <Spinner />
   }
 }
 
