9469b3cc29b1b7e966bf326eec74fc9987d531a1
xaviguasch <xaviguasch@gmail.com>
2020-10-31 12:10:13 +0100

    added icons for winter season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,8 +11,15 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
   const text = season === 'winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!'
+  const icon = season === 'winter' ? 'snowflake' : 'sun'
 
-  return <div>{text}</div>
+  return (
+    <div>
+      <i className={`${icon} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`${icon} icon`}></i>
+    </div>
+  )
 }
 
 export default SeasonDisplay
